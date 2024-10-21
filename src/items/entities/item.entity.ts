fb6a350eb0428174e0ff16b0ad710737d8355056
yeet2042 <arpeggiokongo@gmail.com>
2024-10-22 02:41:48 +0700

    create entity for users

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,9 +1,10 @@
+import { User } from 'src/users/entities/user.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 enum StatusEnum {
-  PENDING = 'pending',
-  APPROVED = 'approved',
-  REJECTED = 'rejected',
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED',
 }
 
 @Entity()
@@ -11,21 +12,27 @@ export class Item {
   @PrimaryGeneratedColumn()
   item_id: number;
 
-  @ManyToOne(() => User, (user) => user.items)
+  @ManyToOne(() => User, (user) => user.items, { nullable: false })
   user: User;
 
-  @ManyToOne(() => Department, (department) => department.items)
+  @ManyToOne(() => Department, (department) => department.items, {
+    nullable: false,
+  })
   department: Department;
 
-  @Column()
+  @Column({ nullable: false })
   title: string;
 
-  @Column()
+  @Column({ nullable: false })
   amount: number;
 
-  @Column()
+  @Column({ nullable: false })
   quantity: number;
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: StatusEnum,
+    default: StatusEnum.PENDING,
+  })
   status: StatusEnum;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import { Item } from 'src/items/entities/item.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+enum Role {
+  REQUESTER = 'REQUESTER',
+  APPROVER = 'APPROVER',
+  ADMIN = 'ADMIN',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @ManyToOne(() => Department, (department) => department.users, {
+    nullable: false,
+  })
+  department: Department;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.REQUESTER,
+  })
+  role: Role;
+
+  @OneToMany(() => Item, (item) => item.user)
+  items: Item[];
+}
