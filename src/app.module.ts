eb7757bd22c575e78c1d460b800cb65131cae13e
yeet2042 <arpeggiokongo@gmail.com>
2024-10-22 01:35:54 +0700

    create items module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
+import { ItemsModule } from './items/items.module';
 import dbConfig from './db/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [dbConfig] }),
     DbModule,
+    ItemsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
