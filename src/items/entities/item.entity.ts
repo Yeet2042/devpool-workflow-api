5efd397fca64c0ff86fee9c9b6c65b3e2c08703a
yeet2042 <arpeggiokongo@gmail.com>
2024-10-22 02:24:07 +0700

    create entity for items

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,31 @@
-export class Item {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+enum StatusEnum {
+  PENDING = 'pending',
+  APPROVED = 'approved',
+  REJECTED = 'rejected',
+}
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  item_id: number;
+
+  @ManyToOne(() => User, (user) => user.items)
+  user: User;
+
+  @ManyToOne(() => Department, (department) => department.items)
+  department: Department;
+
+  @Column()
+  title: string;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  status: StatusEnum;
+}
