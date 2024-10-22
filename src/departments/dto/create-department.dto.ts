87f31e799e39ff2ae21c66de42fbae5d7d9f1123
yeet2042 <arpeggiokongo@gmail.com>
2024-10-22 21:41:00 +0700

    create dto for department

--- src/departments/dto/create-department.dto.ts
+++ src/departments/dto/create-department.dto.ts
@@ -1,1 +1,7 @@
-export class CreateDepartmentDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateDepartmentDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
