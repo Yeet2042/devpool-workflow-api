b0f5fe3e6db6439a61b30b1c351b19f893b1590d
yeet2042 <arpeggiokongo@gmail.com>
2024-10-24 18:49:00 +0700

    implement cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { QueryFailedErrorFilter } from './filters/query-failed-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new QueryFailedErrorFilter(httpAdapter));
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
